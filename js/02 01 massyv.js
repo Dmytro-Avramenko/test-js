6f349b9c1a8d18ec904f524c03a682a395ca32f8
Дмитро Авраменко <105928143+Dmytro-Avramenko@users.noreply.github.com>
2022-08-31 00:04:49 +0300

    31_08

---
+++ js/02 01 massyv.js
@@ -0,0 +1,8 @@
+// Массиви знайомство
+
+const friends = ["Mango", "Kivi", "Poly", "Ajax"];
+
+// console.log(friends[2])
+// останій символ в масиві
+const lastIndex = friends.length - 1;
+console.log(lastIndex);
